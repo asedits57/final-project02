788370de8599dbefc43f20e3b6f6b32915749ea3
AS_edits M <your_github_email@gmail.com>
2026-04-08 02:16:09 +0530

    push updates

--- backend/src/routes/oauthRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import * as oauthController from "../controllers/oauthController";
-
-const router: Router = express.Router();
-
-router.post("/google/callback", oauthController.googleCallback);
-router.post("/github/callback", oauthController.githubCallback);
-
-export default router;
